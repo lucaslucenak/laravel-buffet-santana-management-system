7f388143bfdc6cf93996a9d450912219842298aa
Lucas Lucena <lucas.lucenak@gmail.com>
2023-06-27 18:33:23 -0300

    Update: create cliente font

--- resources/views/components/forms/form.blade.php
+++ resources/views/components/forms/form.blade.php
@@ -5,8 +5,9 @@
     <div class="card-body">
         <form method="POST">
             {{$formRows}}
-            <div>
-                <button class="btn btn--radius-2 btn--red btn-register" type="submit" style='background-color:#eec13b;'>{{$buttonName}}</button>
+            <div class="d-flex justify-content-between">
+                <a href="#" class="btn btn--radius-2 btn--red btn-register" style='background-color:#424039; color:white;'>Voltar</a>
+                <button class="btn btn--radius-2 btn--red btn-register" type="submit" style='background-color:#eec13b; color:white;'>{{$buttonName}}</button>
             </div>
         </form>
     </div>
