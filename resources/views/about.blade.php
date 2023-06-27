59fb80450114b89adcddf7f7b9609f12c490cbac
Lucas Lucena <lucas.lucenak@gmail.com>
2023-06-27 20:33:38 -0300

    Update: about page navs

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
     <div class="wrapper d-flex align-items-stretch">
         <nav id="sidebar">
             <x-sidebar.sidebar>
-                <x-sidebar.sidebar-item class="active" href='#'>
+                <x-sidebar.sidebar-item href='/home'>
                     Home
                 </x-sidebar.sidebar-item>
                 <x-sidebar.sidebar-item href='/events'>
@@ -20,7 +20,7 @@
                 <x-sidebar.sidebar-item href='/help'>
                     Ajuda
                 </x-sidebar.sidebar-item>
-                <x-sidebar.sidebar-item href='/about'>
+                <x-sidebar.sidebar-item class="active" href='#'>
                     Sobre
                 </x-sidebar.sidebar-item>
             </x-sidebar.sidebar>
@@ -30,7 +30,7 @@
         <div id="content" class="p-4 p-md-5">
             <section id="top-navbar">
                 <x-top-navbar.top-navbar>
-                    <x-top-navbar.top-navbar-item href="#" class="nav-item active">
+                    <x-top-navbar.top-navbar-item href="/home" class="nav-item">
                         Home
                     </x-top-navbar.top-navbar-item>
                     <x-top-navbar.top-navbar-item href="/events" class="nav-item">
@@ -42,7 +42,7 @@
                     <x-top-navbar.top-navbar-item href="/help" class="nav-item">
                         Ajuda
                     </x-top-navbar.top-navbar-item>
-                    <x-top-navbar.top-navbar-item href="/about" class="nav-item">
+                    <x-top-navbar.top-navbar-item href="#" class="nav-item active">
                         Sobre
                     </x-top-navbar.top-navbar-item>
                 </x-top-navbar.top-navbar>
