17dcec43624e7121a6e2086158392494fc992ec6
Lucas Lucena <lucas.lucenak@gmail.com>
2023-06-28 01:58:06 -0300

    Update: confirmation card from confirm passwrod reset

--- resources/views/components/confirm-email-sent-modal.blade.php
+++ resources/views/components/confirm-email-sent-modal.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmar Exclusão</h5>
+          <h5 class="modal-title" id="confirmDeleteModalLabel">E-mail de recuperação enviado!</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
             <span aria-hidden="true">&times;</span>
           </button>
