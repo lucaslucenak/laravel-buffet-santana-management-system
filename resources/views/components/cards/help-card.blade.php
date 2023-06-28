da09b82db8d4b6ba4b8ed1abcd872e838202ea1d
Lucas Lucena <lucas.lucenak@gmail.com>
2023-06-27 21:27:45 -0300

    Update: help icons

---
+++ resources/views/components/cards/help-card.blade.php
@@ -0,0 +1,10 @@
+<div class="col-md-4">
+    <div class="card-inner h-100 p-3 d-flex flex-column align-items-center">
+        <div class="icon-container flex-grow-1">
+            <span class="icon" style="font-size: 24px;">{{$icon}}</span>
+        </div>
+        <div class="text-center title-container">
+            <span class="title-text">{{$title}}</span>
+        </div>
+    </div>
+</div>
--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -53,55 +53,32 @@
                     <div class="row d-flex justify-content-center">
                         <div class="col-md-9">
                             <div class="card p-4 mt-3">
-                                <h3 class="heading mt-5 text-center">Hi! How can we help You?</h3>
-                                <div class="d-flex justify-content-center px-5">
-                                    <div class="search"> <input type="text" class="search-input" placeholder="Search..."
-                                            name=""> <a href="#" class="search-icon"> <i
-                                                class="fa fa-search"></i> </a> </div>
-                                </div>
-                                <div class="row mt-4 g-1 px-4 mb-5">
-                                    <div class="col-md-2">
-                                        <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
-                                                src="https://i.imgur.com/Mb8kaPV.png" width="50">
-                                            <div class="text-center mg-text"> <span class="mg-text">Account</span> </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
-                                                src="https://i.imgur.com/ueLEPGq.png" width="50">
-                                            <div class="text-center mg-text"> <span class="mg-text">Payments</span> </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
-                                                src="https://i.imgur.com/tmqv0Eq.png" width="50">
-                                            <div class="text-center mg-text"> <span class="mg-text">Delivery</span> </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
-                                                src="https://i.imgur.com/D0Sm15i.png" width="50">
-                                            <div class="text-center mg-text"> <span class="mg-text">Product</span> </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
-                                                src="https://i.imgur.com/Z7BJ8Po.png" width="50">
-                                            <div class="text-center mg-text"> <span class="mg-text">Return</span> </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="card-inner p-3 d-flex flex-column align-items-center"> <img
-                                                src="https://i.imgur.com/YLsQrn3.png" width="50">
-                                            <div class="text-center mg-text"> <span class="mg-text">Guarantee</span> </div>
-                                        </div>
-                                    </div>
+                                <h3 class="heading mt-2 text-center">Suporte e Manuais</h3>
+                                <h5 class="mt-1 text-center">Suporte e Manuadadasdasdasdasdais</h5>
+                                <div class="row mt-4 g-3 justify-content-center">
+                                    <x-cards.help-card>
+                                        @slot('icon')
+                                        <i class="fa-solid fa-play" style="font-size: 30px;"></i>
+                                        @endslot
+                                        @slot('title')
+                                        Instalação e inicialização do projeto
+                                        @endslot
+                                    </x-cards.help-card>
+                                    <x-cards.help-card>
+                                        @slot('icon')
+                                        <i class="fa-solid fa-book" style="font-size: 30px;"></i>
+                                        @endslot
+                                        @slot('title')
+                                        Como utilizar o sistema desenvolvido
+                                        @endslot
+                                    </x-cards.help-card>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
+            
         </div>
     </div>
 @endsection
